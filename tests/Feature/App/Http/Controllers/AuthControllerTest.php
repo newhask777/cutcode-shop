0653e95a9cf63b4684c4b976ef64913c53c035d5
newhask777 <newhaskellisp@gmail.com>
2024-11-12 20:31:46 +0300

    start testing

---
+++ tests/Feature/App/Http/Controllers/AuthControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\App\Http\Controllers;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+    use RefreshDatabase;
+}
